4a0e482b2b54130e68cb054f1c982edbaba13fbd
madarasz.oli <madaraszoliver@gmail.com>
2025-04-09 11:58:25 +0200

    Munkas class added

---
+++ DeathStar/Munkas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeathStar
+{
+    internal class Munkas
+    {
+        string nev;
+        int allapot;
+        int tempo { get; set; }
+        public bool Elkeszult { get { return (allapot == 100 ? true : false); } }
+        public string Nev { get { return nev; } set { nev = value; } }
+        public int Allapot { get { return allapot; } set { allapot = value; } }
+
+        public bool Figyelik { get; set; }
+
+        public Munkas(string nev, int allapot, int tempo)
+        {
+            this.nev = nev;
+            this.allapot = allapot;
+            this.tempo = tempo;
+        }
+
+        public void Lep()
+        {
+            allapot++;
+            if (!Figyelik)
+            {
+                Thread.Sleep(tempo);
+            }
+            else
+            {
+                Thread.Sleep((int)Math.Round((double)tempo / 2));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Nev} készültség: {this.Allapot}";
+        }
+
+
+    }
+}
