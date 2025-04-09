74b48ac66306fc3dad76a11e269935a2cd2d2437
madarasz.oli <madaraszoliver@gmail.com>
2025-04-09 12:07:13 +0200

    Rohamosztagos added, prop added

---
+++ DeathStar/Rohamosztagos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeathStar
+{
+    internal class Rohamosztagos
+    {
+        static int idnumber = 999;  
+        string id;
+        public readonly Munkas Felugyelve;
+        public string Id { get { return id; }set {id = "FN" + idnumber++; } }
+
+        public Rohamosztagos()
+        {
+                
+        }
+
+
+
+    }
+}
