a4644b72666bc32af69b7b84acb628fcb9ae3009
madarasz.oli <madaraszoliver@gmail.com>
2025-04-10 09:39:29 +0200

    tasks added

--- DeathStar/Program.cs
+++ DeathStar/Program.cs
@@ -7,8 +7,30 @@ namespace DeathStar
         static void Main(string[] args)
         {
             XDocument doc = XDocument.Load("deathstarXML.xml");
+            List<Munkas> l = (from x in doc.Descendants("munkas")
+                              select new Munkas(
+                                  x.Attribute("nev").Value,
+                                  int.Parse(x.Attribute("tempo").Value),
+                                  int.Parse(x.Attribute("allapot").Value)
+                                  )).ToList();
+
+            List<Task> t = new List<Task>();
+            foreach (Munkas m in l)
+            {
+                t.Add(new Task(()=>
+                {
+                    while(!m.Elkeszult)
+                    {
+                        m.Lep();
+                    }
+                }, TaskCreationOptions.LongRunning));
+            }
+
 
             
+
+
+
         }
 
     }
