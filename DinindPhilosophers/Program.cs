f4ca6a92e6684904d34f8fe9641f487ec26214ad
madarasz.oli <madaraszoliver@gmail.com>
2025-04-17 11:12:17 +0200

    Alice and Bob

--- DinindPhilosophers/Program.cs
+++ DinindPhilosophers/Program.cs
@@ -22,12 +22,18 @@
             Random rng = new Random(i + (int)DateTime.Now.Ticks);
             while (true)
             {
+                FM.Kiirt("Hungry", i);                //Hungry
+                FM.pickup(i);
+
+                FM.Kiirt("Eating", i);
+                Thread.Sleep(rng.Next(1000, 3000)); // Eating
+
+                FM.putdown(i);
+
                 FM.Kiirt("Thinking", i);
                 Thread.Sleep(rng.Next(1000, 3000)); // Thinking
 
-                FM.pickup(i);
-                FM.Kiirt("Eating", i);
-                Thread.Sleep(rng.Next(1000, 3000)); // Eating
+
 
                 FM.putdown(i);
             }
